1f0953eb39c535dd4d61c6dacb2e7fcb4b024df1
Taras Rushchak <taraskhust@gmail.com>
2019-05-27 18:22:07 +0300

    animation trancition

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,26 +1,21 @@
 import React, { useState } from 'react';
-import { useSpring, animated } from 'react-spring';
+import { useTransition, animated } from 'react-spring';
 
 const Toggle = () => {
 	const [isToggled, setToggled] = useState(false);
-	const { y, color } = useSpring({
-		// opacity: isToggled ? 0 : 1,
-		// fontSize: isToggled ? '2rem' : '20em',
-		color: isToggled ? 'tomato' : 'white',
-		y: isToggled ? 0 : 1,
-		transform: isToggled ? 'translate3d(0,0,0)' : 'translate3d(0,-50px,0)'
-	})
+	const transition = useTransition(isToggled, null,{
+		from: { opacity: 0 },
+		enter: { opacity: 1 },
+		leave: { opacity: 0 },
+	});
 
 	return (
 			<>
-				<Animatedtitle
-						style={{
-							transform: y.interpolate({
-								range: [0, 0.25, 0.5, 0.75, 1],
-								output: [0, -25, -50, -100, -50]
-							}).interpolate( y => `translate3d(0, ${y}px, 0)`),
-							color
-				}}>Hello</Animatedtitle>
+				{transition.map(({item, key, props}) => (
+						item && <Animatedtitle key={key} style={props}>
+							Hello
+						</Animatedtitle>
+				))}
 				<button className='button is-danger is-rounded is-large'
 				        onClick={() => setToggled(!isToggled)}>Toogle
 				</button>
