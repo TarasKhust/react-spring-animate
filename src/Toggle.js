951a3514f52e69cf5db856f0bb4a4cb82f984feb
Taras Rushchak <taraskhust@gmail.com>
2019-05-27 16:39:12 +0300

    animation

--- src/Toggle.js
+++ src/Toggle.js
@@ -3,17 +3,24 @@ import { useSpring, animated } from 'react-spring';
 
 const Toggle = () => {
 	const [isToggled, setToggled] = useState(false);
-	const fade = useSpring({
+	const { y, color } = useSpring({
 		// opacity: isToggled ? 0 : 1,
 		// fontSize: isToggled ? '2rem' : '20em',
 		color: isToggled ? 'tomato' : 'white',
-		y: isToggled ? 0 : -50,
+		y: isToggled ? 0 : 1,
 		transform: isToggled ? 'translate3d(0,0,0)' : 'translate3d(0,-50px,0)'
 	})
 
 	return (
 			<>
-				<Animatedtitle style={{...fade}}>Hello</Animatedtitle>
+				<Animatedtitle
+						style={{
+							transform: y.interpolate({
+								range: [0, 0.25, 0.5, 0.75, 1],
+								output: [0, -25, -50, -100, -50]
+							}).interpolate( y => `translate3d(0, ${y}px, 0)`),
+							color
+				}}>Hello</Animatedtitle>
 				<button className='button is-danger is-rounded is-large'
 				        onClick={() => setToggled(!isToggled)}>Toogle
 				</button>
