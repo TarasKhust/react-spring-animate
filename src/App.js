507a74cbbb5458bb6c8d81cc395707843488f603
Taras Rushchak <taraskhust@gmail.com>
2019-05-27 13:58:46 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useSpring, animated } from 'react-spring';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+	return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="logo"/>
+					<button className="menu-button">Menu</button>
+				</header>
+			</div>
+	);
+};
+
+export default App;
