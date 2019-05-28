0b76e19590fe4ea292ea0035ad9765de78c090f5
Taras Rushchak <taraskhust@gmail.com>
2019-05-28 14:24:38 +0300

    drop and drug

--- src/Gesture.js
+++ src/Gesture.js
@@ -11,6 +11,7 @@ const Gesture = () => {
 
 	return (
 			<animated.div style={{
+				opacity: xy.interpolate({ map: Math.abs, range: [0, 400], output: [1, 0]}),
 				transform: xy.interpolate((x, y) => `translate3d(${x}px, ${y}px, 0)`)
 			}}{...bind()} className="box"/>
 	);
